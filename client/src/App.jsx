7a2d901e2734753e4ccc512439b57a89ff348c70
AryanKadam1134 <aryankadam3114@gmail.com>
2026-02-11 23:21:03 +0530

    feat: integrate Tailwind CSS and Ant Design, refactor App component for routing
    - Added Tailwind CSS and Ant Design as dependencies.
    - Removed App.css and migrated styles to Tailwind CSS.
    - Refactored App component to use React Router for navigation.
    - Created AdminDashboard and AdminLogin components for admin functionality.
    - Implemented API call in AdminDashboard to fetch social platforms.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+
+import AdminDashboard from "./pages/private/AdminDashboard";
 
 function App() {
-  const [socialPlatforms, setSocialPlatforms] = useState(null);
-
-  const fetchSocialPlatforms = async () => {
-    try {
-      const res = await axios.get(
-        "http://localhost:3000/api/filter/social-platforms",
-      );
-
-      const data = res.data?.data;
-
-      setSocialPlatforms(data);
-      console.log("Social Platforms: ", data);
-    } catch (error) {
-      console.error("Error fetching jokes: ", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchSocialPlatforms();
-  }, []);
-
   return (
-    <div>
-      {socialPlatforms?.map((platform, idx) => (
-        <div key={idx}>{platform?.label}</div>
-      ))}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin" />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/private/AdminDashboard.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function AdminDashboard() {
+  const [socialPlatforms, setSocialPlatforms] = useState(null);
+
+  const fetchSocialPlatforms = async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:3000/api/filter/social-platforms",
+      );
+
+      const data = res.data?.data;
+
+      setSocialPlatforms(data);
+      console.log("Social Platforms: ", data);
+    } catch (error) {
+      console.error("Error fetching jokes: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSocialPlatforms();
+  }, []);
+
+  return (
+    <div>
+      {socialPlatforms?.map((platform, idx) => (
+        <div key={idx}>{platform?.label}</div>
+      ))}
+    </div>
+  );
+}
