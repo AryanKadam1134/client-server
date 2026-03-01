408b128838580f4f23befbd6eb3b31cda1d22a95
AryanKadam1134 <aryankadam3114@gmail.com>
2026-03-02 00:14:41 +0530

    feat: add skill retrieval functionality with category lookup in portfolio controller and routes

--- server/src/controllers/public/portfolio.controller.js
+++ server/src/controllers/public/portfolio.controller.js
@@ -1,6 +1,9 @@
+import mongoose from "mongoose";
+import { Skill } from "../../models/skill.model.js";
 import { SocialAccount } from "../../models/socialAccount.model.js";
 import ApiRes from "../../utils/ApiRes.js";
 import asynchandler from "../../utils/asynchandler.js";
+import ApiError from "../../utils/ApiError.js";
 
 const getUserByUsername = asynchandler(async (req, res) => {
   return res
@@ -21,4 +24,38 @@ const getUserSocialAccounts = asynchandler(async (req, res) => {
     .json(new ApiRes(200, platforms, "Platforms fetched successfully!"));
 });
 
-export { getUserByUsername, getUserSocialAccounts };
+const getSkillWithCategory = asynchandler(async (req, res) => {
+  const skills = await Skill.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user?._id),
+        visibility: true,
+      },
+    },
+    {
+      $lookup: {
+        from: "skillcategories",
+        localField: "categoryId",
+        foreignField: "_id",
+        as: "category",
+      },
+    },
+    {
+      $addFields: {
+        category: {
+          $first: "$category",
+        },
+      },
+    },
+  ]);
+
+  if (!skills) {
+    throw new ApiError(500, "couldn't get all skills!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiRes(200, skills, "skills fetched successfully!"));
+});
+
+export { getUserByUsername, getUserSocialAccounts, getSkillWithCategory };
--- server/src/routes/public/portfolio.routes.js
+++ server/src/routes/public/portfolio.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { findUserByUsername } from "../../middlewares/user.middleware.js";
 import {
+  getSkillWithCategory,
   getUserByUsername,
   getUserSocialAccounts,
 } from "../../controllers/public/portfolio.controller.js";
@@ -15,5 +16,9 @@ portfolioRouter
 portfolioRouter
   .route("/:username/social-platforms")
   .get(findUserByUsername, getUserSocialAccounts);
+
+portfolioRouter
+  .route("/:username/skills")
+  .get(findUserByUsername, getSkillWithCategory);
 
 export default portfolioRouter;
